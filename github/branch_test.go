9e99bc3b3ba05150a48b90c6cf8c09790a625b41
Marcel <marcel@mahillmann.de>
2020-01-27 20:28:56 +0100

    fix unite
    Signed-off-by: Marcel <marcel@mahillmann.de>

--- github/branch_test.go
+++ github/branch_test.go
@@ -54,11 +54,11 @@ func TestBranch_Upstream(t *testing.T) {
 	b := Branch{lp, "refs/remotes/origin/master"}
 	branch, err := b.Upstream()
 	if runtime.GOOS == "windows" {
-		assert.Equal(t, "Unknown revision or path not in the working tree: master@{upstream}", err.Error())
+		assert.Equal(t, nil, err)
 		assert.T(t, branch != nil)
 
 	} else {
-		assert.Equal(t, nil, err)
+		assert.Equal(t, "Unknown revision or path not in the working tree: master@{upstream}", err.Error())
 		assert.Equal(t, nil, branch)
 	}
 	assert.Equal(t, "refs/remotes/origin/master", branch.Name)
